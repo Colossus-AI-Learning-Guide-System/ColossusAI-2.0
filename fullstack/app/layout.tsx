cfcc0cc0c83553e87df5aa1a9fd115e667525441
Akila Senanayake <akilasenanayake22@gmail.com>
2025-02-22 19:33:48 +0530

    Chnage background color to align with web theme

--- fullstack/app/layout.tsx
+++ fullstack/app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import "./styles/globals.css"
 import PageTransition from "./components/PageTransition"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -13,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="noise-overlay" />
         <PageTransition>
           {children}
         </PageTransition>
